a15f1085df4ab8dd378bcb98daec6cd0c12c0268
Was403 <vladimirovichdanil@bk.ru>
2024-04-17 21:52:33 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Deplozile ADB
+>NOTE: *Необходимо **adb** для работы*
+
+Инструмент для резервной копии с Android устройства на Windows PC.
+
+## Using
+* set on in your phone developer\`s settings and click on debug bridge.
+* plug-in your phone to PC
+* Start `main.py` and well done)
+
+## Troubleshooting
+No problems go to google or android documentations
+
+## Issues
+- [ ] Progress bar doesn\`t work
+- [ ] We had not GUI
+- [ ] We had not cool hacker console
+- [ ] Cosplay Watch Dogs
---
+++ main.py
@@ -0,0 +1,28 @@
+import subprocess
+#from tqdm import tqdm
+from strings import FILE_PATH_ON_DEVICE, START_MESSAGE, DESTINATION_PATH_ON_PC
+def is_adb_available():
+    try:
+        subprocess.run(["adb", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+def download_file_from_android(file_path_on_device, destination_path_on_pc):
+    
+    adb_command = f"adb pull {file_path_on_device} {destination_path_on_pc}"
+    
+    try:
+        if not is_adb_available():
+            raise RuntimeError("ADB недоступен. Пожалуйста, убедитесь, что Android Debug Bridge установлен и добавлен в PATH.")
+        
+        subprocess.run(adb_command, shell=True, check=True)
+        print(f"Файл успешно скачан на ПК: {destination_path_on_pc}")
+    except subprocess.CalledProcessError as e:
+        print(f"Произошла ошибка при скачивании файла: {e}")
+
+# Укажите путь к файлу на устройстве Android и путь на ПК, куда файл будет скачан
+
+if __name__ == "__main__":
+    
+    print(START_MESSAGE)
+    download_file_from_android(FILE_PATH_ON_DEVICE, DESTINATION_PATH_ON_PC)
